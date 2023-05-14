9b677a34767493f322112043c38d65e245d8c99a
Katarzyna Bocian (239911) <katbocian98@gmail.com>
2023-05-14 23:45:02 +0200

    Update AlonMatiasSzegedySimplified.scala
    Repaired algorithm

--- src/main/scala/AlonMatiasSzegedySimplified.scala
+++ src/main/scala/AlonMatiasSzegedySimplified.scala
@@ -1,19 +1,23 @@
 import scala.util.Random
 class AlonMatiasSzegedySimplified(streamLength: Int, r: Int) {
-  private val randomIndices = Seq.fill(r)(Random.nextInt(streamLength))
+  private val randomIndices = Array.fill(r)(Random.nextInt(streamLength))
+  private val counts = new Array[Int](r)
+  private val words = new Array[String](r)
   private var position = 0
-  private val counters = scala.collection.mutable.Map[String, Int]().withDefaultValue(0)
 
-  def add(value: String): Unit = {
-    if (randomIndices.contains(position)) counters(value) += 1
+  def add(word: String): Unit = {
     position += 1
+    randomIndices.indices.filter(i => randomIndices(i) == position).foreach { i =>
+      words(i) = word
+      counts(i) += 1
+    }
+    randomIndices.indices.filter(i => words(i) == word && randomIndices(i) < position).foreach { i =>
+      counts(i) += 1
+    }
   }
 
-  def estimateSecondMoment: Double = {
-    2.0 * streamLength * counters.values.map(count => count * count).sum / r - streamLength
-  }
-
-  def estimateThirdMoment: Double = {
-    6.0 * streamLength * streamLength * counters.values.map(count => count * count * count).sum / r - 3 * streamLength * streamLength + 2 * streamLength
+  def estimateMoment(k:Int) : BigInt = {
+    val momentEstimate = counts.map(x => BigInt(math.pow(x, k).toInt) - BigInt(math.pow(x - 1, k).toInt)).sum
+    BigInt(streamLength / r) * momentEstimate
   }
 }
