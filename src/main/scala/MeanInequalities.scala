d7276b094a3796bf30ad478cd5f044fb380595de
Katarzyna Bocian (239911) <katbocian98@gmail.com>
2023-05-14 23:44:50 +0200

    Update MeanInequalities.scala
    Repaired funtions. Random numbers only from Integer range

--- src/main/scala/MeanInequalities.scala
+++ src/main/scala/MeanInequalities.scala
@@ -1,14 +1,14 @@
 import scala.util.Random
 class MeanInequalities(streamLength: Int) {
-  private val randomNumbers: Seq[Int] = Seq.fill(streamLength)(Random.nextInt())
-  private val sortedNumbers = randomNumbers.sorted
+  private val randomNumbers: Seq[Int] = Seq.fill(streamLength)(Random.nextInt(Int.MaxValue)).sorted
+
   val median: Double = if (streamLength % 2 ==0) {
-    (sortedNumbers(streamLength / 2 - 1) + sortedNumbers(streamLength / 2 )) / 2.0
+    (randomNumbers(streamLength / 2 - 1) + randomNumbers(streamLength / 2 )) / 2.0
   } else {
-    sortedNumbers(streamLength / 2)
+    randomNumbers(streamLength / 2)
   }
 
   val m1: Double = randomNumbers.sum.toDouble / streamLength
-  val m2: Double = math.sqrt(randomNumbers.map(n => n * n).sum.toDouble / streamLength)
-  val m3: Double = math.cbrt(randomNumbers.map(n => n * n * n).sum.toDouble / streamLength)
+  val m2: Double = math.sqrt(randomNumbers.map(n => n.toDouble * n.toDouble).sum / streamLength)
+  val m3: Double = math.cbrt(randomNumbers.map(n => n.toDouble * n.toDouble * n.toDouble).sum / streamLength)
 }
